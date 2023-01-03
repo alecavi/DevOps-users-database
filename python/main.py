11c245dfe3937bab6decc2a06d53f3bf5465e9c7
alecavi <acavicchioli@dundee.ac.uk>
2023-01-03 16:31:06 +0100

    recomposed

--- python/main.py
+++ python/main.py
@@ -7,16 +7,16 @@ from routes import router as user_router
 app = FastAPI()
 config = dotenv_values(".env")
 
+
 @app.on_event("startup")
 def startup():
-    app.collection = MongoClient(config["DB_URL"])[config["DB_NAME"]][config["DB_DOC"]]
+    app.collection = MongoClient(config["DB_URL"])[
+        config["DB_NAME"]][config["DB_DOC"]]
+
 
 @app.on_event("shutdown")
 def shutdown():
     app.mongodb_client.close()
 
-@app.get("/")
-async def root():
-    return {"message": "If you're seeing this, I'm not horribly broken"}
 
-app.include_router(user_router, tags=["users"], prefix = "/user")
+app.include_router(user_router, tags=["users"], prefix="/user")
--- python/models.py
+++ python/models.py
@@ -4,50 +4,59 @@ import re
 from typing import List, Set, Type
 from uuid import UUID
 
-# This should ideally be transferred under HTTPS, since there _is_ a password.
-# However, HTTPS requires a domain name, and domain names are not free, nor can they be bought with
-# gcloud's trial credit.
-#
-# That, and it's going to be behind a firewall anyway, so at least it's not wholly insecure
+
 class UserAccessBody(BaseModel):
-    name: str = Field(description = "Username")
-    password: str = Field(description = "Password")
+    # This should ideally be transferred under HTTPS, since there _is_ a password.
+    # However, HTTPS requires a domain name, and domain names are not free, nor can they be bought with
+    # gcloud's trial credit.
+    #
+    # That, and it's going to be behind a firewall anyway, so at least it's not wholly insecure
+    name: str = Field(description="Username")
+    password: str = Field(description="Password")
 
     @validator("name")
     def name_characters(cls: Type["RegisterBody"], v: str):
         if not re.match("[A-Za-z0-9_-]+", v):
-            raise ValidationError("username must contain only letters, numbers, underscores, and dashes")
+            raise ValidationError(
+                "username must contain only letters, numbers, underscores, and dashes")
         return v
-    
+
     @validator("password")
     def password_length(cls: Type["RegisterBody"], v: str):
         if len(v) < 8:
-            raise ValidationError("Password must be at least 8 characters long")
+            raise ValidationError(
+                "Password must be at least 8 characters long")
         return v
 
+
 class UserAccessResponse(BaseModel):
-    id: str = Field(description = "The user's ID")
+    id: str = Field(description="The user's ID")
+
 
 class ListUpdate(str, Enum):
     add = "add"
     remove = "remove"
 
+
 class ListUpdateBody(BaseModel):
-    update: ListUpdate = Field(description = "Whether this operation should add or remove an element")
+    update: ListUpdate = Field(
+        description="Whether this operation should add or remove an element")
 
 
 class Success(BaseModel):
-    success: bool = Field(description = "Whether the action succeded", default = True)
+    success: bool = Field(
+        description="Whether the action succeded", default=True)
 
 
 class UserData(BaseModel):
-    likes: List[UUID] = Field(description = "The videos this user likes")
-    watch_later: List[UUID] = Field(description = "The videos this user has added to their \"watch later\" list")
+    likes: List[UUID] = Field(description="The videos this user likes")
+    watch_later: List[UUID] = Field(
+        description="The videos this user has added to their \"watch later\" list")
 
 
 class HTTPError(BaseModel):
-    status_code: int = Field(description = "Error code")
-    detail: str = Field(description = "Error description")
+    status_code: int = Field(description="Error code")
+    detail: str = Field(description="Error description")
 
     class Config:
         schema_extra = {
